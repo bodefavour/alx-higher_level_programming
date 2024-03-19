94bf16571f0990c899c2d1589c3ccabc846aba43
Bode Favour <bodefavour@gmail.com>
2024-03-18 17:12:23 -0700

    fix the Cpython task, try 1

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <Python.h>
+#include <lists.h>
 
 /**
  * print_python_list_info - prints python list info
@@ -12,13 +12,13 @@ void print_python_list_info(PyObject *p)
 	long int size, i;
 	PyListObject *list;
 	PyObject *item;
-
+	
 	size = Py_SIZE(p);
 	printf("[*] Size of the Python List = %ld\n", size);
-
+	
 	list = (PyListObject *)p;
 	printf("[*] Allocated = %ld\n", list->allocated);
-
+	
 	for (i = 0; i < size; i++)
 	{
 		item = PyList_GetItem(p, i);
