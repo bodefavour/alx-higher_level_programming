96790189d9c3d60d524862f4232cc7f04e1e7710
Bode Favour <bodefavour@gmail.com>
2024-07-26 07:41:52 -0700

    edit the file

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    length = len(my_string)
+def new_in_list(my_list, idx, element):
+    length = len(my_list)
 
-    j = 0
+    new_list = my_list[:]
 
-    new_string = my_string[:]
+    if 0 <= idx < length:
+        new_list[idx] = element
 
-    for i in range(length):
-        if (my_string[i] == 'c' or my_string[i] == 'C'):
-            new_string = new_string[:(i - j)] + my_string[(i + 1):]
-            j += 1
-
-    return (new_string)
+    return (new_list)
