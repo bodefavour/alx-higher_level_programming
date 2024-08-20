231969a96ad44c3d395e3f9974fc6d14b4a19a2b
Bode Favour <bodefavour@gmail.com>
2024-08-20 06:47:59 -0700

    write a javascript script that prints the first argument passed to it and fix semistandard errors

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const firstArgument = process.argv[2];
 if (firstArgument) {
-console.log(firstArgument);
-}
-else {
-console.log('No argument');
+  console.log(firstArgument);
+} else {
+  console.log('No argument');
 }
