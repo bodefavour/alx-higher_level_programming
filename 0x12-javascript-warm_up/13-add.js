096d4103fa5b6534c65c22ee9c0b64c2ed8282ba
Bode Favour <bodefavour@gmail.com>
2024-08-20 11:39:28 -0700

    create a function that adds two integers and handle errors

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-function add(num1, num2) {
-  return num1 + num2;
-}
-
-const num1 = parseInt(process.argv[2]);
-const num2 = parseInt(process.argv[3]);
-const result = add(num1, num2);
-console.log(result);
+exports.add = function (a, b) {
+  return (a + b);
+};
